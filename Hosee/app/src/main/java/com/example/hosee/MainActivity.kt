be5eec5793ad0134d1acd4c0e39d8f7a5e428643
Nguyễn ̄ ThăngxD <nguyenthang17101@gmail.com>
2019-04-04 18:29:16 +0700

    ProjectHosee

--- Hosee/app/src/main/java/com/example/hosee/MainActivity.kt
+++ Hosee/app/src/main/java/com/example/hosee/MainActivity.kt
@@ -1,112 +1,85 @@
 package com.example.hosee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
-import com.example.projectxd.R
+import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.conten_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
 
-
-
-
-
-open class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.example.projectxd.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        (map as SupportMapFragment).getMapAsync(this)
         setSupportActionBar(toolbar)
-//        showDetails()
 
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, com.example.projectxd.R.string.navigation_drawer_open, com.example.projectxd.R.string.navigation_drawer_close
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
-        drawer_layout.addDrawerListener(toggle)
+        drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-
-        nav_view.setNavigationItemSelectedListener(this)
+        navView.setNavigationItemSelectedListener(this)
     }
-//    var count = 0
-//    fun showDetails() {
-//        val thread = object : Thread() {
-//
-//            override fun run() {
-//                try {
-//                    while (!this.isInterrupted) {
-//                        Thread.sleep(1000)
-//                        runOnUiThread {
-//                            // update TextView here!
-//                        }
-//                    }
-//                } catch (e: InterruptedException) {
-//                }
-//
-//            }
-//        }
-//
-//        thread.start()
-//    }
-
-
-
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
-
     }
-//
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.activity_drawer_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        when (item.itemId) {
-////            R.id.action_settings -> return true
-//            else -> return super.onOptionsItemSelected(item)
-//        }
-//    }
+
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
-
+        // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_history -> {
-
+            R.id.nav_home -> {
                 // Handle the camera action
             }
-
-            R.id.nav_notification -> {
+            R.id.nav_gallery -> {
 
             }
-            R.id.nav_help -> {
+            R.id.nav_slideshow -> {
 
             }
             R.id.nav_sos -> {
 
             }
-            R.id.nav_introduce -> {
+            R.id.nav_tools -> {
 
             }
-        }
 
-        drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
